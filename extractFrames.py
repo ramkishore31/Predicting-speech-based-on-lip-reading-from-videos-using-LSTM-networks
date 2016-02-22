bef40068b4d5303fce4a625a440b6c569d76e507
ramkishore31 <ramkishore31@gmail.com>
2016-02-22 13:41:00 -0800

    first commit

---
+++ extractFrames.py
@@ -0,0 +1,33 @@
+import cv2
+import os
+cnt1 = 1
+cnt2 = 1
+
+speaker_list = next(os.walk('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_videos/'))[1]
+for speaker in speaker_list:
+    print "Extracting frames for speaker ",cnt1
+    for filename in os.listdir('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_videos/'+str(speaker)+'/'):
+        if cnt2 % 25 == 0:
+            print cnt2," videos done for ",speaker
+        if filename.endswith('.mpg'):
+            frame_count = 1
+            vc = cv2.VideoCapture('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_videos/'+str(speaker)+'/' + filename)
+            if vc.isOpened():
+                rval , frame = vc.read()
+            else:
+                rval = False
+
+            while rval:
+                rval, frame = vc.read()
+                if not os.path.exists('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_frames/'+str(speaker)+'/'):
+                    os.makedirs('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_frames/'+str(speaker)+'/')
+
+                if not os.path.exists('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_frames/'+str(speaker)+'/'+ str(filename) +'/'):
+                    os.makedirs('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_frames/'+str(speaker)+'/'+ str(filename) +'/')
+
+                cv2.imwrite('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_frames/'+str(speaker)+'/'+ str(filename) +'/' +str(frame_count)+'.jpg',frame)
+                frame_count = frame_count + 1
+                cv2.waitKey(1)
+            vc.release()
+        cnt2 += 1
+    cnt1 += 1
