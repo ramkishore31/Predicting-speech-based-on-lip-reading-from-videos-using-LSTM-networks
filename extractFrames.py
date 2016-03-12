8f4264ad5597a6c3606b7fb4161e14531fe58195
ramkishore31 <ramkishore31@gmail.com>
2016-03-11 18:56:36 -0800

    Code to extract frames

--- extractFrames.py
+++ extractFrames.py
@@ -1,13 +1,14 @@
 import cv2
 import os
 
-speaker_list = next(os.walk('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_videos/'))[1]
+source_path = '/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_videos/'
+destination_path = '/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_frames/'
+speaker_list = next(os.walk(source_path))[1]
 for speaker in speaker_list:
-    print "Extracting frames for speaker ",cnt1
-    for filename in os.listdir('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_videos/'+str(speaker)+'/'):
+    for filename in os.listdir(source_path + str(speaker)+'/'):
         if filename.endswith('.mpg'):
             frame_count = 1
-            vc = cv2.VideoCapture('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_videos/'+str(speaker)+'/' + filename)
+            vc = cv2.VideoCapture(source_path + str(speaker)+'/' + filename)
             if vc.isOpened():
                 rval , frame = vc.read()
             else:
@@ -15,13 +16,13 @@ for speaker in speaker_list:
 
             while rval:
                 rval, frame = vc.read()
-                if not os.path.exists('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_frames/'+str(speaker)+'/'):
-                    os.makedirs('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_frames/'+str(speaker)+'/')
+                if not os.path.exists(destination_path + str(speaker)+'/'):
+                    os.makedirs(destination_path + str(speaker)+'/')
 
-                if not os.path.exists('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_frames/'+str(speaker)+'/'+ str(filename) +'/'):
-                    os.makedirs('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_frames/'+str(speaker)+'/'+ str(filename) +'/')
+                if not os.path.exists(destination_path + str(speaker)+'/'+ str(filename) +'/'):
+                    os.makedirs(destination_path + str(speaker)+'/'+ str(filename) +'/')
 
-                cv2.imwrite('/Users/ramkishore31/Neural Networks/final_project/dataset/lowquality_frames/'+str(speaker)+'/'+ str(filename) +'/' +str(frame_count)+'.jpg',frame)
+                cv2.imwrite(destination_path + str(speaker)+'/'+ str(filename) +'/' +str(frame_count)+'.jpg',frame)
                 frame_count = frame_count + 1
                 cv2.waitKey(1)
             vc.release()
